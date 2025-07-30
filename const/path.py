61e7421fef717d27c091c1f8ebc61d2162503cca
CZL <zhulichengdut@foxmail.com>
2025-07-31 01:54:42 +0800

    modify PD_PATH_POSES

--- const/path.py
+++ const/path.py
@@ -8,7 +8,7 @@ OUT_PATH = '/caa/Homes01/iballester/log/motion_encoder/out/'
 PREPROCESSED_DATA_ROOT_PATH = f'{NDRIVE_PROJECT_ROOT}/data'
 
 # PD
-PD_PATH_POSES='/data/iballester/datasets/Public_PD/C3Dfiles_processed'
+PD_PATH_POSES='/czl_ssd/Public_PD/C3Dfiles_processed_new'
 PD_PATH_LABELS='/data/iballester/datasets/Public_PD/PDGinfo.csv'
 
 CHECKPOINT_ROOT_PATH =  '/caa/Homes01/iballester/log/motion_encoder/out/motionbert/finetune_6_pd,json/1/models'
